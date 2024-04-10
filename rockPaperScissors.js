951c48a75adf92d8c5255c54c90ed3926d7d331a
temery1213 <temery1213@gmail.com>
2024-04-10 12:03:21 -0500

    playRound function added

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return Math.floor(Math.random() * 3)
@@ -15,7 +16,39 @@ let computerSelection = getComputerChoice();
         computerSelection = "scissors";
     }
 
+   // const button = document.querySelector("button");
+//let playerSelection = button.addEventListener("click"); 
 
 
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === "rock" && computerSelection === "paper"){
+        return computerScore++;
+    
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return computerScore++;
+        
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return computerScore++;
+    
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return playerScore++;
+    
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return playerScore++;
+    
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return playerScore++;
+    } else {
+        return;
+    }
+
+
+} 
+  const playerSelection = "paper";
+  console.log(playRound(playerSelection, computerSelection));
 
 console.log(computerSelection);
+console.log(playerSelection);
+console.log(playerScore);
+console.log(computerScore);
